5e1ff60a8e0b0fd6eca8e857987100c5d13e49d7
hesheng <heshengyx@163.com>
2020-01-04 16:27:14 +0800

    java样例项目-ThreadLocal

---
+++ src/main/java/com/hsheng/core/thread/ThreadLocalTimer.java
@@ -0,0 +1,32 @@
+package com.hsheng.core.thread;
+
+/**
+ * @description 线程本地变量
+ * @author hesheng
+ * @date 2020/1/4 16:07
+ * @since TODO
+ */
+public class ThreadLocalTimer {
+
+    private final static ThreadLocal<Long> TIMES = new ThreadLocal<Long>() {
+        @Override
+        protected Long initialValue() {
+            return System.currentTimeMillis();
+        }
+    };
+
+    /**
+     * 计时开始时间
+     */
+    public final static void begin() {
+        TIMES.set(System.currentTimeMillis());
+    }
+
+    /**
+     * 计算结束时长
+     * @return
+     */
+    public final static long end() {
+        return System.currentTimeMillis() - TIMES.get();
+    }
+}
